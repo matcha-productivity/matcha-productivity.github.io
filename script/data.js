9a76821700ed4286e2ed43c8d844d6bccddc537e
matcha-productivity <81783412+matcha-productivity@users.noreply.github.com>
2021-04-01 20:57:21 -0400

    Add files via upload

---
+++ script/data.js
@@ -0,0 +1,156 @@
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+
+
+
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD1YBSA5DcwjK92qsMeiYturKLA1Q2EQdw",
+    authDomain: "matcha-82995.firebaseapp.com",
+    projectId: "matcha-82995",
+    storageBucket: "matcha-82995.appspot.com",
+    messagingSenderId: "167793790051",
+    appId: "1:167793790051:web:70537a57f4af22fd1b3c56",
+    measurementId: "G-V0FTX11XTB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var signedin = false;
+  var initsnap = false;
+  var data;
+
+  $('#signin').click(()=>{
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+  
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  });
+
+  $('#logout').click(()=>{
+    firebase.auth().signOut().then(() => {
+        console.log('sign out successful')
+      }).catch((error) => {
+        // An error happened.
+      });
+  })
+  
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        signedin=true;
+        $('#signin').hide();
+        $('#logout').show();
+        $('table').show();
+
+        var profile = firebase.auth().currentUser;
+        $('#name').text(profile.displayName);
+        $('#image').attr('src', profile.photoURL);
+        $('.profile').css('opacity','1');
+
+        db.collection("users").doc(firebase.auth().currentUser.uid).get().then(doc => {
+            if (doc.exists) {
+                data = doc.data();
+                init();
+            } else {
+                data = {};
+                init();
+            }
+        });
+
+        db.collection("users").doc(profile.uid)
+            .onSnapshot((doc) => {
+                var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+                if (source == 'Server' && initsnap){
+                    data = doc.data();
+                    refresh();
+                }
+            });
+
+
+            
+      // ...
+    } else {
+        signedin=false;
+        data = {};
+        $('.task').remove();
+        $('#signin').show();
+        $('#logout').hide();
+        $('table').hide();
+        $('#signin').focus();
+        $('.profile').css('opacity','0');
+      // User is signed out
+      // ...
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+  /************************* FIRESTORE */
+
+// Initialize Cloud Firestore through Firebase
+  
+  var db = firebase.firestore();
+
+
+function save(){
+    if (signedin){
+        db.collection("users").doc(firebase.auth().currentUser.uid).set(data)
+        .then(() => {
+            //console.log("Document written with ID: " + firebase.auth().currentUser.uid);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+    }
+}
+
+$('#backup').click(()=>{
+    if (signedin){
+        db.collection("backup").doc(firebase.auth().currentUser.uid).set(data)
+        .then(() => {alert('Backup sucessful')})
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+    }
+})
+
+
+$('#save').click(()=>{save(); refresh()})
+
---
+++ script/index.js
@@ -0,0 +1,251 @@
+
+/*var data = {
+    20201201: [
+        {
+            id: "15342affss424",
+            val: "Sign up for SAT",
+            type: "task",
+            complete: true,
+        },
+    ],
+    20210322: [
+        {
+            id: "15342affss424",
+            val: "Drink Milk tea",
+            type: "task",
+            complete: true,
+        },
+    ],
+    20210202: [
+        {
+            id: "15342affss424",
+            val: "Scary",
+            type: "task",
+            complete: true,
+        },
+        {
+            id: "15342affss424",
+            val: "Remember to die",
+            type: "task",
+            complete: true,
+        },
+    ],
+    20210203: [
+        {
+            id: "15342affss424",
+            val: "Buy cake for Sandy",
+            type: "task",
+            complete: true,
+        },
+    ],
+    20210328: [
+        {
+            id: "15342424",
+            val: "go take a shower",
+            type: "task",
+            complete: true,
+        },
+        {
+            id: "123422",
+            val: "Finish math homework",
+            type: "task",
+            complete: false,
+        }
+    ],
+    20210329: [
+        {
+            id: "1323232",
+            val: "Finish Biology",
+            type: "task",
+            complete: false,
+        }
+    ]
+
+}*/
+
+
+
+function init(){
+
+    $('.day').remove();
+
+    var now = new Date();
+    var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    var lastSunday = new Date(today.setDate(today.getDate()-today.getDay()));
+    var row; var col; var datecycle = lastSunday;
+
+    // creating the calendar
+    for (row=0; row<5; row++){
+        var week = $('<tr>').appendTo('.table');
+        for (col=0;col<7;col++){
+            var dateid = formatDate(datecycle);
+            var dayblock = $('<td>').appendTo(week).addClass('day').attr('id', dateid);
+            var topbar = $('<div>').addClass('top').appendTo(dayblock);
+            var daynum = $('<p>').text(datecycle.getDate()).appendTo(topbar);
+
+            if (dateid in data){ // check if date already in data
+                //console.log(dateid + ' already exists.')
+            } else { // if not then make new "date"
+                data[dateid] = [];
+            }
+
+            var tools = $('<div>').addClass('tools').appendTo(topbar);
+            var addbtn = $('<div>').appendTo(tools).attr('onclick', 'createNote('+dateid+')');
+            $('<ion-icon>').attr('name','add-outline').appendTo(addbtn);
+            datecycle.setDate(datecycle.getDate() + 1);
+        }
+    }
+
+    
+
+    $('#samplecon').hide();
+    refresh();
+}
+
+function refresh(){
+    
+    $('.task').remove();
+    // iterating over data to render existing notes
+    jQuery.each(data, (i, val) => { // i = dateid, val = object with date tasks
+        jQuery.each(val, (j, task) => { // j= taskindex, task = object with task data
+            createNote(i, task.val, task.id, task.complete);
+        })
+    });
+    
+}
+
+
+var strokeback2;
+var strokeback;
+var strokenow;
+
+function createNote(dayselected, val, idnum, complete){
+    initsnap = true;
+    var id;
+    if (idnum != null){id = idnum} // already existing timestamp id
+    else { // if this is a new task, create  a new timestamp id
+        id = Date.now();
+        data[dayselected].push({
+            id: id,
+            val: "",
+            type: "task",
+            complete: false,
+        });
+        save();
+    };
+    
+    var task; // finding the 'task' in the data and setting a variable to allow edits
+    var index;
+    jQuery.each(data, (i, date) => { // iterating to find the task
+        jQuery.each(date, (k, datetask)=>{
+            if (datetask.id == id){
+                task = data[dayselected][k];
+                index = k;
+            }
+        })
+    });
+    
+    var note;
+    if( $('#'+dayselected).length ) // checking if the date element exists, or is not on the calendar
+    {
+        note = $('#sample').clone().addClass('task').appendTo($('#'+dayselected)).attr('id',  id);
+    } else { 
+        // if the task does not exist
+        var head;
+        if(! $('.H-'+dayselected).length){
+            head = $('<div>').html('<h3>'+dateToNatural(stringToDate(dayselected))+'</h3>').prependTo('#archived').addClass('H-'+dayselected+' datehead');  
+        }
+        note = $('#sample').clone().addClass('task').appendTo('.H-'+dayselected).attr('id',  id);
+        
+    }
+    
+    //******************************************************* */ field editor
+    var field = (note.find('p')).focus().html(val);
+    field.keypress((e)=>{ // if user types in task
+        strokenow = String.fromCharCode(e.keyCode);
+        // console.log(strokenow, strokeback, strokeback2);
+        if (strokenow == ' ' && strokeback == 'r' && strokeback2 == '/') {
+            e.preventDefault();
+            window.getSelection().modify("extend", "backward", "character");
+            window.getSelection().deleteFromDocument();
+            (note.find('p')).css('background-color','#ff7a7a');
+        }
+        if (e.shiftKey && e.keyCode == 13){ //******************** another new Task
+            e.preventDefault();
+            createNote(dayselected);
+        }
+
+        strokeback2 = strokeback;
+        strokeback = strokenow;
+    });
+    field.keydown((e)=>{
+        if (e.keyCode == 46 || e.keyCode == 8){ //******************** deleting a task
+            if (field.text()==''){
+                e.preventDefault();  
+                note.remove();
+                data[dayselected].splice(index, 1);
+                save();
+            }
+        }
+    });
+    field.keyup(e=>{
+        task.val = field.html();
+        save();
+    })
+
+    //******************************************************* */ checkbox
+    var checkbox = (note.find('input'));
+    if (complete){checkbox.attr('checked','checked');}
+    checkbox.click(()=>{
+        if(checkbox.prop('checked')) {
+            task.complete = true;
+        } else {
+            task.complete = false;
+        };
+        save();
+    });
+}
+
+
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('');
+}
+
+function parseDate(datestring){
+    var utc = new Date(datestring).toUTCString();
+    return utc;
+}
+
+function stringToDate(formatted){ // input: YYYYMMDD  ouput: year
+    var dateString  = formatted;
+    var year        = dateString.substring(0,4);
+    var month       = dateString.substring(4,6);
+    var day         = dateString.substring(6,8);
+
+    return new Date(year, month-1, day);
+}
+
+function dateToNatural(date){ // input: Date Obj   output: April 4
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+    return monthNames[date.getMonth()] + ' ' + date.getDate();
+}
+
+
+
+
+
+
+
